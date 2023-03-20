1a1f74b4d79d3278971288f392b9450bc2f5dfa1
Prasad Gupta <Ayush.PrasadGupta@ellucian.com>
2023-03-20 11:38:44 +0530

    deploy

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'f0654e9207msh61012bc978717f0p1d095djsnb6d446429421',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
